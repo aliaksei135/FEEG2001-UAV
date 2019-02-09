1478b01ee64135c4d5aeb4e966035a1cb2ee13bf
Aliaksei Pilko <aliakseipilko1@gmail.com>
2019-02-09 19:46:52 +0000

    Remade guidance logic

--- utilities/headingStateTransitionFunc.m
+++ utilities/headingStateTransitionFunc.m
@@ -1,10 +1,10 @@
-function xhat = headingStateTransitionFunc(x, v)
+function xhat = headingStateTransitionFunc(V_b)
 %HEADINGSTATETRANSITIONFUNC State Transition Function for Heading sensor
 %fusion
 
 %#codegen
 
 dt = 1; %1 sec time step
-xhat = x + atan2(v(1), v(2))/dt; %step with angle between X and Y velocities
+xhat = x + atan2(V_b(1), V_b(2))/dt; %step with angle between X and Y velocities
 
 end
