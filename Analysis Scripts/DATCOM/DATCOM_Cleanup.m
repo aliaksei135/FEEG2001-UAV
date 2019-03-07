c6bdbc994095b99a985f794a819b7be282df83b4
Aliaksei Pilko <aliakseipilko1@gmail.com>
2019-03-07 18:30:54 +0000

    Impl EKF filtering and update aero consts

--- Analysis Scripts/DATCOM/DATCOM_Cleanup.m
+++ Analysis Scripts/DATCOM/DATCOM_Cleanup.m
@@ -4,7 +4,7 @@ for n = 1:length(datcom)
         for m = 1:datcom{n}.nmach
             for h = 1:datcom{n}.nalt
                 datcom{n}.(aerotab{k})(:,m,h) = datcom{n}.(aerotab{k})(1,m,h);
-                datcom{n}.('cnb')(:,m,h) = 0.15;
+                datcom{n}.('cnb')(:,m,h) = 0.09896;
             end
         end
     end
