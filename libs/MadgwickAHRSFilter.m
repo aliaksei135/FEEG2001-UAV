b1f0efbe6bfb117e300851cbb4d51e212edb16ac
Aliaksei Pilko <aliakseipilko1@gmail.com>
2019-03-17 15:17:52 +0000

    Fixed AHRS filter and aligned axes

--- libs/MadgwickAHRSFilter.m
+++ libs/MadgwickAHRSFilter.m
@@ -1,47 +1,38 @@
 classdef MadgwickAHRSFilter < matlab.System
-
+    
+    %#codegen
+    
     % Public, tunable properties
     properties
-        SamplePeriod = 0.02;
+        SamplePeriod = 0.01;
         Beta = 0.1;               	% algorithm gain
     end
     
     methods(Access = protected)
-
-        function stepImpl(obj, Gyroscope, Accelerometer, Magnetometer)
-            global q;
-            
+        
+        function quat = stepImpl(obj, Gyroscope, Accelerometer, Magnetometer, q)
+            if (sum(isnan(Magnetometer)) ~= 0)
+                quat = UpdateIMU(obj, Gyroscope, Accelerometer, q);
+            else
+                quat = Update(obj, Gyroscope, Accelerometer, Magnetometer, q);
+            end
+        end
+    end
+    
+    methods
+        function q = Update(obj, Gyroscope, Accelerometer, Magnetometer, q)
             % Normalise accelerometer measurement
             if(norm(Accelerometer) == 0), return; end	% handle NaN
             Accelerometer = Accelerometer / norm(Accelerometer);	% normalise magnitude
-
+            
             % Normalise magnetometer measurement
-            if(norm(Magnetometer) == 0)
-                % Gradient decent algorithm corrective step
-                F = [2*(q(2)*q(4) - q(1)*q(3)) - Accelerometer(1)
-                    2*(q(1)*q(2) + q(3)*q(4)) - Accelerometer(2)
-                    2*(0.5 - q(2)^2 - q(3)^2) - Accelerometer(3)];
-                J = [-2*q(3),	2*q(4),    -2*q(1),	2*q(2)
-                    2*q(2),     2*q(1),     2*q(4),	2*q(3)
-                    0,         -4*q(2),    -4*q(3),	0    ];
-                step = (J'*F);
-                step = step / norm(step);	% normalise step magnitude
-                
-                % Compute rate of change of quaternion
-                qDot = 0.5 * quatmultiply(q, [0 Gyroscope(1) Gyroscope(2) Gyroscope(3)]) - obj.Beta * step';
-                
-                % Integrate to yield quaternion
-                q = q + qDot * obj.SamplePeriod;
-                q = q / norm(q); % normalise quaternion
-                return;
-            end
+            if(norm(Magnetometer) == 0), return; end	% handle NaN
             Magnetometer = Magnetometer / norm(Magnetometer);	% normalise magnitude
             
-            mag = [0 Magnetometer(1) Magnetometer(2) Magnetometer(3)];
             % Reference direction of Earth's magnetic feild
-            h = quatmultiply(q, quatmultiply(mag, quatconj(q)));
+            h = quatmultiply(q, quatmultiply([0 Magnetometer], quatconj(q)));
             b = [0 norm([h(2) h(3)]) 0 h(4)];
-
+            
             % Gradient decent algorithm corrective step
             F = [2*(q(2)*q(4) - q(1)*q(3)) - Accelerometer(1)
                 2*(q(1)*q(2) + q(3)*q(4)) - Accelerometer(2)
@@ -60,11 +51,32 @@ classdef MadgwickAHRSFilter < matlab.System
             
             % Compute rate of change of quaternion
             qDot = 0.5 * quatmultiply(q, [0 Gyroscope(1) Gyroscope(2) Gyroscope(3)]) - obj.Beta * step';
-
+            
             % Integrate to yield quaternion
             q = q + qDot * obj.SamplePeriod;
-            q = q / quatnorm(q); % normalise quaternion
+            q = q / norm(q); % normalise quaternion
         end
-
+        function q = UpdateIMU(obj, Gyroscope, Accelerometer, q)
+            % Normalise accelerometer measurement
+            if(norm(Accelerometer) == 0), return; end	% handle NaN
+            Accelerometer = Accelerometer / norm(Accelerometer);	% normalise magnitude
+            
+            % Gradient decent algorithm corrective step
+            F = [2*(q(2)*q(4) - q(1)*q(3)) - Accelerometer(1)
+                2*(q(1)*q(2) + q(3)*q(4)) - Accelerometer(2)
+                2*(0.5 - q(2)^2 - q(3)^2) - Accelerometer(3)];
+            J = [-2*q(3),	2*q(4),    -2*q(1),	2*q(2)
+                2*q(2),     2*q(1),     2*q(4),	2*q(3)
+                0,         -4*q(2),    -4*q(3),	0    ];
+            step = (J'*F);
+            step = step / norm(step);	% normalise step magnitude
+            
+            % Compute rate of change of quaternion
+            qDot = 0.5 * quatmultiply(q, [0 Gyroscope(1) Gyroscope(2) Gyroscope(3)]) - obj.Beta * step';
+            
+            % Integrate to yield quaternion
+            q = q + qDot * obj.SamplePeriod;
+            q = q / norm(q); % normalise quaternion
+        end
     end
 end
